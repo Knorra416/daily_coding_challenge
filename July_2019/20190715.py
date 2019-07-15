15306823178503ddfd8131ccc5114e94c068842b
alex.knorr <alex.knorr@slalom.com>
2019-07-15 12:13:06 -0600

    better solution!

--- July_2019/20190715.py
+++ July_2019/20190715.py
@@ -6,26 +6,35 @@
 def cons(a, b):
     def pair(f):
         return f(a, b)
+
     return pair
 # Implement car and cdr.
 
 
+def make_list(a, b):
+    """
+    this function takes two items (pairs) and returns a list
+    :param a:
+    :param b:
+    :return:
+    """
+    my_list = [a, b]
+    return my_list
+
+
 def car(f):
     """
-    this function returns the min of the pairs provided from a function.
+    this function returns the first element of the pairs provided from a function.
     :param f: function (cons)
     :return: int
     """
-    return f(min)
+    return f(make_list)[0]
 
 
 def cdr(f):
     """
-    this function returns the min of the pairs provided from a function.
+    this function returns the last element of the pairs provided from a function.
     :param f: function (cons)
     :return: int
     """
-    return f(max)
-
-# These aren't quite right. Don't catch the case where pairs are not ordered as min/max.
-
+    return f(make_list)[1]
