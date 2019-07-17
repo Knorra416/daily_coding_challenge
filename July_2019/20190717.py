a397e71289dbc24e93f3fb4aaec6bf34ee55d598
alex.knorr <alex.knorr@slalom.com>
2019-07-17 13:33:09 -0400

    this feels hard. First (failed) shot.

--- July_2019/20190717.py
+++ July_2019/20190717.py
@@ -9,3 +9,26 @@
 # You can assume that the messages are decodable.
 # For example, '001' is not allowed.
 
+mapping = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z']
+
+
+def decoder(message, mapping):
+    """
+    this function returns the count of
+    :param message:
+    :return:
+    """
+    decoded_options = []
+    for i in range(len(message)):
+        remaining = message[i+1:]
+        if len(remaining) < 3:
+            decode = mapping[i+1] + mapping[remaining]
+            decoded_options.append(decode)
+        else:
+            continue
+
+    return len(decoded_options)
+
+
+
