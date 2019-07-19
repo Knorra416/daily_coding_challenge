ec60b2e9cbc6294922d7d74c8adfff15c9e4b0fd
alex.knorr <alex.knorr@slalom.com>
2019-07-19 09:39:02 -0600

    solution from yesterday.

--- July_2019/20190718.py
+++ July_2019/20190718.py
@@ -9,3 +9,23 @@
 #  / \
 # 1   1
 
+
+# I don't really understand trees/roots. This is the solution.
+def is_unival(root):
+    return unival_helper(root, root.value)
+
+
+def unival_helper(root, value):
+    if root is None:
+        return True
+    if root.value == value:
+        return unival_helper(root.left, value) and unival_helper(root.right, value)
+    return False
+
+
+def count_unival_subtrees(root):
+    if root is None:
+        return 0
+    left = count_unival_subtrees(root.left)
+    right = count_unival_subtrees(root.right)
+    return 1 + left + right if is_unival(root) else left + right
