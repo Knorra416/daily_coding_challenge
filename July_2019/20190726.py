a82e2ccf90394b8839c16c68bf51456ecb070ad9
alex.knorr <alex.knorr@slalom.com>
2019-07-26 10:41:56 -0600

    my solution added.

--- July_2019/20190726.py
+++ July_2019/20190726.py
@@ -6,3 +6,23 @@
 # i is guaranteed to be smaller than or equal to N.
 
 
+def order_id_logging(order_id_list, n, logger):
+    """
+    This function records order_ids into a log.
+    :param order_id_list: list of order_ids
+    :param n: int number of n last orders to put into log
+    :param logger: logger object
+    :return: the nth element from the log
+    """
+    for i in order_id_list:
+        if order_id_list.index(i) <= n:
+            record(i)
+        else:
+            continue
+
+    return get_last(n)
+
+
+
+
+
