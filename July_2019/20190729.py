bd7484f8ff968dc5706a0f7c0355c83097efe72a
alex.knorr <alex.knorr@slalom.com>
2019-07-30 09:51:47 -0600

    added yesterdays solution and todays problem.

--- July_2019/20190729.py
+++ July_2019/20190729.py
@@ -5,4 +5,16 @@
 # return the minimum cost which achieves this goal.
 
 
+# Solution
+def build_houses(matrix):
+    n = len(matrix)
+    k = len(matrix[0])
+    solution_matrix = [[0] * k]
 
+    # Solution matrix: matrix[i][j] represents the minimum cost to build house i with color j.
+    for r, row in enumerate(matrix):
+        row_cost = []
+        for c, val in enumerate(row):
+            row_cost.append(min(solution_matrix[r][i] for i in range(k) if i != c) + val)
+        solution_matrix.append(row_cost)
+    return min(solution_matrix[-1])
