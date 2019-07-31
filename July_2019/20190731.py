ff29e56f581992c4d15ebfe50c341cf8863487ec
alex.knorr <alex.knorr@slalom.com>
2019-07-31 13:48:37 -0600

    working on solution.

--- July_2019/20190731.py
+++ July_2019/20190731.py
@@ -2,3 +2,25 @@
 # find the minimum number of rooms required.
 
 # For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
+
+
+def min_classrooms(class_times):
+    """
+    given a list of (start,end) arrays return the min number of rooms required to meet the intervals.
+    :param class_times:
+    :return:
+    """
+    total_rooms = 1
+    list_schedule = []
+    for index, interval in enumerate(class_times):
+        start = interval[0]
+        end = interval[1]
+        diff = end - start
+
+    return list_schedule
+
+
+
+
+
+
